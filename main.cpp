854e24bbfb8c08f7fc1774dd44c82a1a63ed4a21
KimyeonJin <ygk313@naver.com>
2022-02-28 19:52:27 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int user_id = -1;
+
 int main(){
 
     int menu_num = 0;
+
+    cout<<"대한민국 No.1 자동차 플랫폼 CARMANY 입니다.\n";
+    cout<<"메뉴를 선택해주세요.(1. 차량구매 2. 차량 추천 3. 마이페이지)";
     
     cin >> menu_num;
-
 
     return 0;
 }
