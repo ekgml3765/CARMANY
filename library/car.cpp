ee77f303e077126eb89185c66146030ad9908bfb
Dahui Hwang <ekgml3765@naver.com>
2022-03-02 01:01:10 +0900

    feat : 자동차 map,vector static 변수 선언, 자동차 상세보기 기능 구현

--- library/car.cpp
+++ library/car.cpp
@@ -2,27 +2,35 @@
 #include <sstream>
 
 /*다희*/
-//전역
-vector<Car> carList = Car::openCarFile();
 
-//객체 출력
+//전역변수
+//vector<Car> car_list = Car::openCarFile();
+
+//static 변수 선언
+vector<Car> Car::car_list_v;
+map<string, Car> Car::car_list_m;
+
+//생성자
+Car::Car(){};
+
+//객체출력
 void Car::print(){
     cout << car_id << " " << brand << " " << name << " " << min_price
     << " " << max_price << " " << type << " " << engine << " " << date
     << " " << color << " " << size << " " << efficency
-    << " " << people << " " << stock << " " << total_stock <<endl;
+    << " " << people << " " << stock << " " << total_stock;
 }
 
-//자동차 파일 읽어오기
-vector<Car> Car::openCarFile(){
+//자동차 파일 읽어서 vector, map에 저장
+bool Car::openCarFile(){
 
-    vector<Car> carList;
-    //ifstream fin("C:\\Users\\user\\Documents\\GitHub\\data\\car.txt"); //window
-    ifstream fin("./car.txt");
+    ifstream fin("C:\\Users\\user\\Documents\\GitHub\\data\\car.txt"); //window
+    //ifstream fin("./car.txt"); //linux
 
     // 파일 열기 실패
     if(!fin) { 
 		cout << "Can't Open File";
+        return false;
 	}
 
     string line;
@@ -49,19 +57,42 @@ vector<Car> Car::openCarFile(){
 	    car.people = stoi(list[11]);        
 	    car.stock = stoi(list[12]);  	  
 	    car.total_stock = stoi(list[13]);
-        carList.push_back(car);
+        //자료구조 저장
+        car_list_v.push_back(car);
+        car_list_m[list[0]] = car;
 	}
-    return carList;
+    fin.close();
+    return true;
 }
 
 //자동차 리스트
 bool Car::getCarList(int category, int page, bool filter){
-    for(int i = 0; i < carList.size(); i++){
-        carList[i].print();
+
+    int total_cars = car_list_v.size();
+    cout << endl;
+    cout << "|  총 " << total_cars << "건 조회";
+    cout << "필터 [인기순/출시순/가격순]  |" << endl;
+
+    for(int i = 0; i < total_cars; i++){
+        cout << "|       " ; 
+        car_list_v[i].print();
+        cout << "          |" << endl;
     }
+    cout << "|--------------------------------------------------------------------------|" << endl;
     return true;
 }
 
+//자동차 상세보기
+bool Car::getCarInfo(string car_id){
+    if(car_list_m.find(car_id) == car_list_m.end()) 
+            return false;
+    else{
+         Car car = car_list_m[car_id];
+         car.print();
+         cout << endl;
+    }
+    return true;
+}
 
 
 //준식
--- main.cpp
+++ main.cpp
@@ -3,70 +3,130 @@
 using namespace std;
 
 int user_id = -1;
-//extern vector<Car> carList = Car::openCarFile();
+
 
 int main(){
-
-    int menu_num = 0;
-    cout << "대한민국 No.1 자동차 플랫폼 CARMANY 입니다.\n";
-    cout << "메뉴를 선택해주세요.(1. 차량구매 2. 차량 추천 3. 마이페이지)" << endl;
-    cin >> menu_num;
-
-    user_id = 1;
-    Car car;
+   
     
-    switch(menu_num){
-        
-        
-        //로그아웃
-        case -1:{
-            cout << "로그아웃!" << endl;
-            break;
-        }
+    vector<string> menu_list = {"차량 구매", "차량 추천", "마이페이지"};
 
-
-        //차량 구매
-        case 1:{
-            int num = 0;
-            string ment = "";
-            string selectMent = "메뉴를 선택해주세요 (0.전체, 1.차종별, 2.엔진별, 3.자동차명, 4. 필터, 5. 페이지 이동, 6.상세보기) >>";
-            while(num != -1){
-                if(num == 0){
-                    car.getCarList(0, 1);
-                    ment = "전체 자동차 리스트 입니다. ";
-                }else if(num == 1){
-                    car.getCarList(1, 1);
-                    ment = "차종별 자동차 리스트 입니다. ";
-                }else if(num == 2){
-                    car.getCarList(2, 1);
-                    ment = "엔진별 자동차 리스트 입니다. ";
-                }else if(num == 3){
-                    car.getCarList(3, 1);
-                    ment = "자동차명 리스트 입니다. ";
-                }else if(num == 4){
-                }
-                else if(num == 5){
-                }
-                else{
-                   
-                }
-                cout << ment << selectMent << endl;
-                cin >> num;             
+    //자동차
+    Car car;
+    Car::openCarFile();
+    user_id = 1;
+    int menu_num = 0;
+    while(menu_num != -1){
+        //상단 메뉴 출력        
+        switch(menu_num){
+            //메인화면
+            default:{
+                    cout << "|--------------------------------------------------------------------------|" << endl;
+                    cout << "|      ";
+                    for (int i = 0; i < menu_list.size(); i++)
+                    {
+                        cout << menu_list[i] << " ";   
+                    }
+                    cout << "|" << endl;
+                    cout << "|--------------------------------------------------------------------------|" << endl;
+                    cout << "|                                                                          |" << endl;
+                    cout << "|                                                                          |" << endl;
+                    cout << "|  .d8008b.  .d8008b.  .d8008b.  .d888888b.  .d8008b.  .d8008b.  dP    dP  |" << endl;
+                    cout << "|  88        88    88  88    88  88  88  88  88    88  88    88  dP    dP  |" << endl;
+                    cout << "|  88        88    88  8800808P  88  88  88  88    88  88    88  dPYYYYdP  |" << endl;
+                    cout << "|  88        88YYYY88  8800      88  88  88  88YYYY88  88    88     00     |" << endl;
+                    cout << "|  88        88    88  88  00    88  88  88  88    88  88    88     00     |" << endl;
+                    cout << "|  888008b.  88    88  88   000  88  88  88  88    88  88    88     00     |" << endl;
+                    cout << "|                                                                          |" << endl;
+                    cout << "|                        _____.-------__            신차 살 땐             |" << endl;
+                    cout << "|                      /                '--.               카매니          |" << endl;
+                    cout << "|             ________/                     '--.                           |" << endl;
+                    cout << "|            /*    __                  __       |  ~~~@@@@                 |" << endl;
+                    cout << "|           |____ /  \\ ______________ /  \\ ____--  ~~~@@                   |" << endl;
+                    cout << "|                 \\__/                \\__/                                 |" << endl;
+                    cout << "|--------------------------------------------------------------------------|" << endl;
+                    cout << endl;
+                    cout << "대한민국 No.1 자동차 플랫폼 CARMANY 입니다.\n";
+                    cout << "메뉴를 선택해주세요.(1. 차량구매 2. 차량 추천 3. 마이페이지)" << endl;
+                break;
             }
-            break;
-        }
 
-        //차량 추천
-        case 2:{
-            break;
-        }
+            //1. 차량 구매
+            case 1:{
+                menu_list = {"전체", "차종별", "엔진", "자동차명"};
+                    cout << "|--------------------------------------------------------------------------|" << endl;
+                    cout << "|      ";
+                    for (int i = 0; i < menu_list.size(); i++)
+                    {
+                        cout << menu_list[i] << " ";   
+                    }
+                    cout << "|" << endl;
+                    cout << "|--------------------------------------------------------------------------|" << endl;           
+                
+                int car_menu_num = 1;
+                string ment = "";
+                while(car_menu_num != -1){
+                    //상세보기
+                    if(car_menu_num == 0){
+                        string car_id;
+                        cout << "조회할 번호를 입력하세요:";
+                        cin >> car_id;
+                        if(!car.getCarInfo(car_id))
+                            cout << "해당 번호는 없는 번호입니다." << endl;
+                    }
+                    //전체
+                    else if(car_menu_num == 1){
+                        car.getCarList(1, 1);
+                        ment = "전체 자동차 리스트 입니다. ";
+                       
+                    }
+                    //차종별
+                    else if(car_menu_num == 2){
+                        car.getCarList(2, 1);
+                        ment = "차종별 자동차 리스트 입니다. ";
+                        
+                    }
+                    //엔진별
+                    else if(car_menu_num == 3){
+                        car.getCarList(3, 1);
+                        ment = "엔진별 자동차 리스트 입니다. ";
+                        
+                    }
+                    //자동차명
+                    else if(car_menu_num == 4){
+                        car.getCarList(4, 1);
+                        ment = "자동차명 자동차 리스트 입니다. ";
+                    }
+                    //필터
+                    else if(car_menu_num == 5){
+                        car.getCarList(5, 1);
+                    }
+                    //페이지 이동
+                    else if(car_menu_num == 6){
+                    
+                    }
+                    cout << ment << "메뉴를 선택해주세요 (0.상세보기 1.전체, 2.차종별, 3.엔진별, 4.자동차명, 5.필터적용, 6. 페이지 이동, 7.차량 구매) >>" << endl;
+                    cin >> car_menu_num;             
+                }
+                break;
+            }
 
 
-        //마이페이지
-        case 3:{
-            break;
+            //2. 차량 추천
+            case 2:{
+                
+               break; 
+            }
+
+
+            //3. 마이페이지
+            case 3:{
+                
+               break; 
+            }
         }
+        cin >> menu_num; //사용자 입력
     }
+    cout << "로그아웃. 종료합니다!" << endl;
 
     return 0;
 }
