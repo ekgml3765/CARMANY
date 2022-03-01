fe9de70f4eac3313345b96a85e1f69b9540d1fe2
KimyeonJin <ygk313@naver.com>
2022-03-01 19:43:19 +0900

    feat : 파일에 사용자 추가

--- library/user.cpp
+++ library/user.cpp
@@ -8,3 +8,24 @@
 
 
 //연진
+#include "user.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+extern int active_user_id;
+extern string username;
+extern userLinkedList userlist;
+
+void appendInFile(int userid, string id, string password, string phone, string name, string age, int sex){
+	ofstream fout;
+
+	fout.open("user.txt", ios::app);
+	if (fout.is_open()){
+		fout<<"\n"<<userid<<" "<<id<<" "<<password<<" "<<phone<<" "<<name<<" "<<age<<" "<<sex;
+	}
+	fout.close();
+}
